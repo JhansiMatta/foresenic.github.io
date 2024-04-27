f6f1bdb46a76a6e79745c16e3a23602e13b248df
JhansiMatta <168255772+JhansiMatta@users.noreply.github.com>
2024-04-26 23:26:19 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // This code runs when the document is loaded
+    alert("Welcome to the Forensic Evidence Management and Investigation System!");
+});
